92d2da847f1bf3ffa82050bd32a4bec139b7a9c9
Dmytro01b <96636950+Dmytro01b@users.noreply.github.com>
2024-08-22 14:59:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+document.body.classList.add("_pc");
+
+// Меню бургер
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle("_lock");
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    });
+}
